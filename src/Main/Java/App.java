c0c298339d515fe962072a3cda2472ebd35739a1
Jose Miguel <jmqc77@gmail,com>
2024-10-09 14:50:52 -0600

    Segudno Commit

--- src/Main/Java/App.java
+++ src/Main/Java/App.java
@@ -1,8 +1,5 @@
 package Main.Java;
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Main.Java.DataBase.DataBaseconnector;
@@ -12,21 +9,6 @@ public class App {
         try {
             Connection connection = DataBaseconnector.getConnection();
             System.out.println("Conexión exitosa a la base de datos");
-
-            // Consultar y mostrar los datos de los clientes
-            String selectQuery = "SELECT * FROM Clientes";
-            try (PreparedStatement selectStatement = connection.prepareStatement(selectQuery)) {
-                ResultSet resultSet = selectStatement.executeQuery();
-
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String nombre = resultSet.getString("nombre");
-                    String apellidoPaterno = resultSet.getString("apellido_paterno");
-                    String apellidoMaterno = resultSet.getString("apellido_materno");
-                    System.out.printf("ID: %d, Nombre: %s %s %s%n", id, nombre, apellidoPaterno, apellidoMaterno);
-                }
-            }
-
             connection.close();
         } catch (SQLException e) {
             System.out.println("Error al conectar a la base de datos: " + e.getMessage());
