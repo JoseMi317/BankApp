018e62d6926a3926ab9c15d0e8e4acd2fb784086
Jose Miguel <jmqc77@gmail,com>
2024-10-27 17:50:38 -0600

    first commit

--- src/Main/Java/DataBase/Querys/Tables2.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE Usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_id INT,
-    nombre_usuario VARCHAR(50) UNIQUE,
-    contraseña_hash VARCHAR(128),
-    rol VARCHAR(20),
-    FOREIGN KEY (cliente_id) REFERENCES Clientes(id)
-);
--- src/Main/Java/DataBase/Querys/ViewClients.sql
+++ src/Main/Java/DataBase/Querys/ViewClients.sql
@@ -1,1 +1,1 @@
-SELECT * FROM Transacciones;
+SELECT * FROM Cuentas;
